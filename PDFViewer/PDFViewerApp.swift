9421426593d2c0b4bd7297ca1966ee8e2cf106ca
vincenzpascarella <vincenz.pascarella@gmail.com>
2023-02-14 11:16:00 +0100

    Initial Commit

---
+++ PDFViewer/PDFViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFViewerApp.swift
+//  PDFViewer
+//
+//  Created by Vincenzo Pascarella on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PDFViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
